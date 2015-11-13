1124b97dacf5171f184c503b949a33957f364b2b
Joey Eremondi <joey@eremondi.com>
2015-11-12 20:10:22 -0800

    Add coord expansion

--- Array/MultiDim.elm
+++ Array/MultiDim.elm
@@ -22,13 +22,32 @@ internal 1D array.
 flattenCoords : List.Safe.Safe Int n -> List.Safe.Safe Int n -> Int
 flattenCoords safeDims safeCoords =
   let
-    dims = List.Safe.toList safeDims
-    coords = List.Safe.toList safeCoords
-    dimensionOffsets = List.scanl (\x y -> x * y) 1 dims
+    dimensionOffsets =
+      List.Safe.scanl (\x y -> x * y) 1 safeDims
+      |> List.Safe.tail
     addOffset (offset, coord) resultSoFar =
       (offset * coord) + resultSoFar
+    offsetCoordPairs =
+      List.Safe.map2 (,) dimensionOffsets safeCoords
   in
-    List.foldr addOffset 0 <| List.map2 (,) dimensionOffsets coords
+    List.foldl addOffset 0 <| List.Safe.toList offsetCoordPairs
+
+
+{- Given the dimensions of an array,
+and an integer,
+find the corresponding coordinates that flatten
+to that integer
+-}
+
+expandCoords : List.Safe.Safe Int n -> Int -> List.Safe.Safe Int n
+expandCoords dims flatCoord =
+  let
+    dimensionOffsets = List.Safe.tail <| List.Safe.scanl (\x y -> x * y) 1 dims
+    --Do some modular arithmetic to find the right place
+    mapFn (offset, lastRemainder ) =
+      (lastRemainder // offset, lastRemainder % offset )
+  in
+    List.Safe.mapl mapFn flatCoord dimensionOffsets
 
 
 {-| Given a SafeList [n1, n2, ...] of array dimensions,
@@ -38,4 +57,20 @@ repeat dims elem =
   let
     arrSize = dims |> List.Safe.toList |> List.product
   in
-    MD {dims = dims, arr = Array.repeat arrSize elem}
+    MD
+    { dims = dims
+    , arr = Array.repeat arrSize elem}
+
+
+initialize
+  :  List.Safe.Safe Int n
+  -> (List.Safe.Safe Int n -> a)
+  -> MultiDim a n
+initialize dims initFn =
+  let
+    arrSize = dims |> List.Safe.toList |> List.product
+    intFn = (expandCoords dims) >> initFn
+  in
+    MD
+    { dims = dims
+    , arr = Array.initialize arrSize intFn}
